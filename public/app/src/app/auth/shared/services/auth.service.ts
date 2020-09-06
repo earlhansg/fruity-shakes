69c732e29ea8e89522f0cda974e663c88738587f
Earl Hans Genoso <earlhansg@github.com>
2020-09-06 19:36:53 +0800

    #15 Created Service for Auth Module

---
+++ public/app/src/app/auth/shared/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable, Inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, throwError, of } from 'rxjs';
+
+import { RestService } from '@shared/service';
+import { HttpMethodEnum } from '@shared/enums';
+
+@Injectable({ providedIn: 'root' })
+export class AuthService extends RestService {
+  url = '/user';
+
+  constructor(http: HttpClient,
+              @Inject('API_URL') protected baseUrl: string) { super(http, baseUrl); }
+
+  login(entryId: string): Observable<any> {
+    return this.request(this.url, HttpMethodEnum.POST, entryId); 
+  }
+
+}
