eb8e63ef3849f7cc101c87ee3c3a55c96479dbff
Earl Hans Genoso <earlhansg@github.com>
2020-09-12 14:15:19 +0800

    #19 Created form builder for paymenForm with basic validation requires user to input money > cart total

--- public/app/src/app/order/shared/components/payment-modal/payment-modal.component.ts
+++ public/app/src/app/order/shared/components/payment-modal/payment-modal.component.ts
@@ -1,4 +1,9 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+import { ReceiptModalComponent } from '@app/order/shared/components/receipt-modal/receipt-modal.component';
+import { CartService } from '@app/order/shared/services/cart.service';
 
 @Component({
     selector: 'app-payment-modal',
@@ -6,11 +11,34 @@ import { Component, ViewEncapsulation } from '@angular/core';
     styleUrls: [ './payment-modal.component.scss' ],
     encapsulation: ViewEncapsulation.None
 })
-export class PaymentModalComponent {
+export class PaymentModalComponent implements OnInit {
 cartTotal: number;
 cartSubTotal: number;
 cartTax: number;
 
-constructor() {}
+bsModalRef: BsModalRef;
+config = {
+    animated: true,
+    keyboard: true,
+    backdrop: true,
+    ignoreBackdropClick: false,
+    class: "modal-dialog-centered custom-modal"
+};
+
+paymentForm: FormGroup;
+
+constructor(private fb: FormBuilder,
+            private modalService: BsModalService,
+            private cartService: CartService) {}
+
+ngOnInit() {
+    this.paymentForm = this.fb.group({
+        money: ['', [Validators.required, Validators.min(this.cartTotal)]]
+    })
+}
+
+enterPayment() {
+    console.log(this.paymentForm.get('money').value);
+}
 
 }
