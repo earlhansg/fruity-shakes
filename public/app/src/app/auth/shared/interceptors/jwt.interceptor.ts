8a6a37e64986933198146ff59d1fb07aacbcc5c3
Earl Hans Genoso <earlhansg@github.com>
2020-09-13 22:43:20 +0800

    #20 Created a method when token expires need to redirect to login and remove data in localstorage

--- public/app/src/app/auth/shared/interceptors/jwt.interceptor.ts
+++ public/app/src/app/auth/shared/interceptors/jwt.interceptor.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
+
+    constructor(private authService: AuthService, private router: Router) {}
+
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
         let currentUser = JSON.parse(localStorage.getItem('currentUser'));
@@ -15,6 +21,17 @@ export class JwtInterceptor implements HttpInterceptor {
             });
         }
 
-        return next.handle(request);
+        return next.handle(request)
+        .pipe(
+            catchError((error: HttpErrorResponse) => {
+                if(error.status === 403) {
+                    console.log('Interceptor code 403');
+                    this.authService.logout();
+                    this.router.navigateByUrl('auth')
+                    return throwError(error);
+                }
+                return throwError(error);
+            })
+        );
     }
 }
