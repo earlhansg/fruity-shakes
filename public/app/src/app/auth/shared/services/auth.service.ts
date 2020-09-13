3116959989fb350405245a877f33d0ff462d9d2d
Earl Hans Genoso <earlhansg@github.com>
2020-09-13 22:54:06 +0800

    #20 install JwtHelperService and created methods isAuthenticated and getToken for AuthGuard

--- public/app/src/app/auth/shared/services/auth.service.ts
+++ public/app/src/app/auth/shared/services/auth.service.ts
@@ -6,6 +6,11 @@ import { Observable } from 'rxjs';
 import { RestService } from '@shared/service';
 import { HttpMethodEnum } from '@shared/enums';
 
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+const jwtHelper = new JwtHelperService();
+
+
 @Injectable({ providedIn: 'root' })
 export class AuthService extends RestService {
   url = '/user/login';
@@ -15,6 +20,17 @@ export class AuthService extends RestService {
 
   login(entryId: number): Observable<any> {
     return this.request(this.url, HttpMethodEnum.POST, {entryId}); 
+  }
+
+  getToken(): string {
+    let loggedUser = JSON.parse(localStorage.getItem('currentUser'));
+    let token = loggedUser && loggedUser.token;
+    return token ? token : '';
+  }
+
+  isAuthenticated(): boolean {
+    const token = this.getToken();
+    return token != '' ? !jwtHelper.isTokenExpired(token) : false;
   }
 
   logout(): void {
