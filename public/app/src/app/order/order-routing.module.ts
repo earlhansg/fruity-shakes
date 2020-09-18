15850a62ff725064c959dd22d1db4ef3810b839c
Earl Hans Genoso <earlhansg@github.com>
2020-09-18 11:59:55 +0800

    #26 Clean code Order module

--- public/app/src/app/order/order-routing.module.ts
+++ public/app/src/app/order/order-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-//component
+
 import { OrderComponent } from './container/order.component';
 
 const routes: Routes = [
@@ -26,6 +26,7 @@ const routes: Routes = [
     ]
   }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
