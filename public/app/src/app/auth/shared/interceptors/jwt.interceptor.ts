32d482ebe3df1504e46c8a8ca4cd68131d778d51
Earl Hans Genoso <earlhansg@github.com>
2020-09-18 10:55:58 +0800

    #26 Clean code JwtInterceptor

--- public/app/src/app/auth/shared/interceptors/jwt.interceptor.ts
+++ public/app/src/app/auth/shared/interceptors/jwt.interceptor.ts
@@ -1,37 +1,39 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor, HttpErrorResponse } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
+
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { AuthService } from '../services/auth.service';
-import { Router } from '@angular/router';
+
+import { AuthService } from '@app/auth/shared/services/auth.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
-    constructor(private authService: AuthService, private router: Router) {}
 
-    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // add authorization header with jwt token if available
-        let currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        if (currentUser && currentUser.token) {
-            request = request.clone({
-                setHeaders: { 
-                    Authorization: `Bearer ${currentUser.token}`
-                }
-            });
-        }
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) { }
 
-        return next.handle(request)
-        .pipe(
-            catchError((error: HttpErrorResponse) => {
-            // the token is expired then user should login again 
-                if(error.status === 403) {
-                    this.authService.logout();
-                    this.router.navigateByUrl('auth/login');
-                    return throwError(error);
-                }
-                return throwError(error);
-            })
-        );
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    if (currentUser && currentUser.token) {
+      req = req.clone({
+        setHeaders: { Authorization: `Bearer ${currentUser.token}` }
+      });
     }
+    return next.handle(req)
+      .pipe(
+        catchError((error: HttpErrorResponse) => {
+          if(error.status === 403 || error.status === 401) {
+            this.authService.logout();
+            this.router.navigateByUrl('auth/login');
+            return throwError(error);
+          }
+          return throwError(error);
+        })
+      );
+  }
+
 }
