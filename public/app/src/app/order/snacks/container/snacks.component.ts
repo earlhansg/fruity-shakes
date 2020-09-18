7257d9b73e6a2e3376694df3a0b1494748905402
Earl Hans Genoso <earlhansg@github.com>
2020-09-18 11:55:12 +0800

    #26 Clean code SnacksComponent

--- public/app/src/app/order/snacks/container/snacks.component.ts
+++ public/app/src/app/order/snacks/container/snacks.component.ts
@@ -1,41 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-// service
-import { SnackService } from '@app/order/shared/services/snack.service';
-import { CartService } from '@app/order/shared/services/cart.service';
-// rxjs 
+
 import { Observable } from 'rxjs';
-// model
-import { Order } from '@app/order/shared/models/order.model';
-import { Cart } from '@app/order/shared/models/cart.model';
-// icon
+
 import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
+import { SnackService } from '@app/order/shared/services/snack.service';
+import { CartService } from '@app/order/shared/services/cart.service';
+
+import { Order } from '@app/order/shared/models/order.model';
+import { Cart } from '@app/order/shared/models/cart.model';
+
+
 @Component({
-    selector: 'app-snacks',
-    templateUrl: './snacks.component.html',
-    styleUrls: [ './snacks.component.scss' ]
+  selector: 'app-snacks',
+  templateUrl: './snacks.component.html',
+  styleUrls: ['./snacks.component.scss']
 })
 export class SnacksComponent implements OnInit {
 
-snacks$: Observable<Order[]>;
-faSpinner = faSpinner;
+  snacks$: Observable<Order[]>;
 
-constructor(private snackService: SnackService,
-            private cartService: CartService) {}
+  faSpinner = faSpinner;
 
-ngOnInit() {
+
+  constructor(
+    private snackService: SnackService,
+    private cartService: CartService
+  ) { }
+
+  ngOnInit(): void {
     this.snacks$ = this.snackService.getSnacks();
-}
+  }
 
-onAddItem(item: Cart) {
+  onAddItem(item: Cart): void {
     const isExist = this.cartService.existInCart(item._id);
-
     if(isExist) {
-        const cartItem = this.cartService.existInCart(item._id, true);
-        const index = this.cartService.itemIndex(item._id);
-        this.cartService.updateItemQuantity(cartItem.quantity, index);
+      const cartItem = this.cartService.existInCart(item._id, true);
+      const index = this.cartService.itemIndex(item._id);
+      this.cartService.updateItemQuantity(cartItem.quantity, index);
     }
     else this.cartService.addtoCart(item);
-}
+  }
 
 }
