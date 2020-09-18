e32190a59d499fd7e64cc54e45a825ffd25fa811
Earl Hans Genoso <earlhansg@github.com>
2020-09-18 11:08:09 +0800

    #26 Clean code AuthService

--- public/app/src/app/auth/shared/services/auth.service.ts
+++ public/app/src/app/auth/shared/services/auth.service.ts
@@ -3,23 +3,27 @@ import { HttpClient } from '@angular/common/http';
 
 import { Observable } from 'rxjs';
 
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 import { RestService } from '@shared/service';
 import { HttpMethodEnum } from '@shared/enums';
-
-import { JwtHelperService } from '@auth0/angular-jwt';
 
 const jwtHelper = new JwtHelperService();
 
 
 @Injectable({ providedIn: 'root' })
 export class AuthService extends RestService {
+
   url = '/user/login';
 
-  constructor(http: HttpClient,
-              @Inject('API_URL') protected baseUrl: string) { super(http, baseUrl); }
+
+  constructor(
+    http: HttpClient,
+    @Inject('API_URL') protected baseUrl: string
+  ) { super(http, baseUrl) }
 
   login(entryId: number): Observable<any> {
-    return this.request(this.url, HttpMethodEnum.POST, {entryId}); 
+    return this.request(this.url, HttpMethodEnum.POST, {entryId});
   }
 
   logout(): void {
