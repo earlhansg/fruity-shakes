d21dfb47b4a5ceec5ff64f1268ae702fb6207cfa
Earl Hans Genoso <earlhansg@github.com>
2020-09-19 10:01:52 +0800

    #26 Clean code PaymentModalComponent

--- public/app/src/app/order/shared/components/payment-modal/payment-modal.component.ts
+++ public/app/src/app/order/shared/components/payment-modal/payment-modal.component.ts
@@ -1,67 +1,68 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-// ngx-bootstrap 
-import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
-// component 
-import { ReceiptModalComponent } from '@app/order/shared/components/receipt-modal/receipt-modal.component';
-// rxjs 
+
 import { BehaviorSubject } from 'rxjs';
-// service
+
+import { BsModalService, BsModalRef } from 'ngx-bootstrap/modal';
+
 import { CartService } from '@app/order/shared/services/cart.service';
 import { ReceiptService } from '@app/order/shared/services/receipt.service';
-// model
 import { Cart } from '@app/order/shared/models/cart.model';
 
+import { ReceiptModalComponent } from '@app/order/shared/components/receipt-modal/receipt-modal.component';
+
 @Component({
-    selector: 'app-payment-modal',
-    templateUrl: './payment-modal.component.html',
-    styleUrls: [ './payment-modal.component.scss' ],
-    encapsulation: ViewEncapsulation.None
+  selector: 'app-payment-modal',
+  templateUrl: './payment-modal.component.html',
+  styleUrls: ['./payment-modal.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class PaymentModalComponent implements OnInit {
-cartTotal: number;
-cartSubTotal: number;
-cartTax: number;
-cartItems: Cart[];
-config: any;
 
-bsModalRef: BsModalRef;
+  isClose = new BehaviorSubject<boolean>(false)
 
-paymentForm: FormGroup;
-isClose = new BehaviorSubject<boolean>(false)
+  paymentForm: FormGroup;
+  bsModalRef: BsModalRef;
+  cartTotal: number;
+  cartSubTotal: number;
+  cartTax: number;
+  cartItems: Cart[];
+  config: any;
 
-constructor(private fb: FormBuilder,
-            private modalService: BsModalService,
-            private cartService: CartService,
-            private receiptService: ReceiptService) {}
 
-ngOnInit() {
+  constructor(
+    private fb: FormBuilder,
+    private modalService: BsModalService,
+    private cartService: CartService,
+    private receiptService: ReceiptService
+  ) { }
+
+  ngOnInit(): void {
     this.cartItems = this.cartService.form.get('items').value;
     this.paymentForm = this.fb.group({
-        money: ['', [Validators.required, Validators.min(this.cartTotal)]]
+      money: ['', [Validators.required, Validators.min(this.cartTotal)]]
     })
-}
+  }
 
-async enterPayment() {
+  async enterPayment() {
     const cashReceived = this.paymentForm.get('money').value;
     const change = cashReceived - this.cartTotal;
     const initialState = {
-        total: this.cartTotal, 
-        subTotal: this.cartSubTotal, 
-        tax: this.cartTax,
-        cashReceived,
-        change,
-        items: this.cartItems
+      total: this.cartTotal,
+      subTotal: this.cartSubTotal,
+      tax: this.cartTax,
+      cashReceived,
+      change,
+      items: this.cartItems
     };
-
     const payment =  await this.receiptService.addReceipt(initialState);
     if(payment) {
-        this.bsModalRef = this.modalService.show(ReceiptModalComponent, 
-            Object.assign(this.config, {initialState}));
-        this.cartService.resetCartForm();
-        this.paymentForm.reset();
-        this.isClose.next(true);
+      this.bsModalRef = this.modalService
+        .show(ReceiptModalComponent, Object.assign(this.config, {initialState}));
+      this.cartService.resetCartForm();
+      this.paymentForm.reset();
+      this.isClose.next(true);
     }
-}
+  }
 
 }
