b162c0543c8274cb120efffe22d23433850f44ca
Earl Hans Genoso <earlhansg@github.com>
2020-09-19 10:07:52 +0800

    #26 Clean code ReceiptModalComponent

--- public/app/src/app/order/shared/components/receipt-modal/receipt-modal.component.ts
+++ public/app/src/app/order/shared/components/receipt-modal/receipt-modal.component.ts
@@ -1,27 +1,27 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-// ngx-bootstrap 
+import { Component, ViewEncapsulation } from '@angular/core';
+
 import { BsModalRef } from 'ngx-bootstrap/modal';
-// model
+
 import { Cart } from '@order/shared/models/cart.model';
 
-@Component({
-    selector: 'app-receipt-modal',
-    templateUrl: './receipt-modal.component.html',
-    styleUrls: [ './receipt-modal.component.scss' ],
-    encapsulation: ViewEncapsulation.None
-})
-export class ReceiptModalComponent implements OnInit{
 
-title: string;
-total: number;
-subTotal: number;
-tax: number;
-cashReceived: number;
-change: number;
-items: Cart[];
- 
-constructor(public bsModalRef: BsModalRef) {}
- 
-ngOnInit() {}
+@Component({
+  selector: 'app-receipt-modal',
+  templateUrl: './receipt-modal.component.html',
+  styleUrls: ['./receipt-modal.component.scss'],
+  encapsulation: ViewEncapsulation.None
+})
+export class ReceiptModalComponent {
+
+  title: string;
+  total: number;
+  subTotal: number;
+  tax: number;
+  cashReceived: number;
+  change: number;
+  items: Cart[];
+
+
+  constructor(public bsModalRef: BsModalRef) { }
 
 }
