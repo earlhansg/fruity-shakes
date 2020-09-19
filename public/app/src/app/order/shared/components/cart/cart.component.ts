d588af4837d294f28dd7d357a4bb82a500c7da15
Earl Hans Genoso <earlhansg@github.com>
2020-09-19 09:03:24 +0800

    #26 Clean code CartComponent

--- public/app/src/app/order/shared/components/cart/cart.component.ts
+++ public/app/src/app/order/shared/components/cart/cart.component.ts
@@ -1,82 +1,82 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
-import { FormGroup } from '@angular/forms';
-// service
-import { CartService } from '@order/shared/services/cart.service';
-// rxjs 
+import { Component, Input, Output, EventEmitter, OnDestroy } from '@angular/core';
+import { AbstractControl, FormGroup } from '@angular/forms';
+
 import { Observable, BehaviorSubject, combineLatest, Subscription } from 'rxjs';
 import { tap, map, share, startWith } from 'rxjs/operators';
-// model 
+
+import { CartService } from '@order/shared/services/cart.service';
+
 import { Cart } from '@order/shared/models/cart.model';
 
+
 @Component({
-    selector: 'app-cart',
-    templateUrl: './cart.component.html',
-    styleUrls: [ './cart.component.scss' ]
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.scss']
 })
-export class CartComponent {
+export class CartComponent implements OnDestroy {
 
-@Input()
-parent: FormGroup;
+  @Input()
+  parent: FormGroup;
 
-@Output()
-removeItem = new EventEmitter<any>();
+  @Output()
+  removeItem = new EventEmitter<any>();
+  @Output()
+  pay = new EventEmitter<any>();
 
-@Output()
-pay = new EventEmitter<any>();
+  subscription$: Subscription;
+  tax$ = new BehaviorSubject<number>(0);
 
-todayDate = new Date();
+  todayDate = new Date();
+  displaySubTotal: number;
+  displayTotal: number;
 
-displaySubTotal: number;
-displayTotal: number;
-tax$ = new BehaviorSubject<number>(0);
 
-subscription: Subscription;
-
-constructor(private cartService: CartService) {
-    this.subscription = combineLatest( [this.subTotal, this.total])
-    .subscribe((value) => {
+  constructor(private cartService: CartService) {
+    this.subscription$ = combineLatest([this.subTotal, this.total])
+      .subscribe(value => {
         this.displaySubTotal = value[0];
         this.displayTotal = value[1];
-    })
-}
+      })
+  }
 
-ngOnDestroy() {
-    this.subscription.unsubscribe();
-}
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
+  }
 
-get orders() {
+  get orders(): AbstractControl[] {
     return this.cartService.items;
-}
+  }
 
-removeToCart(group, index) {
+  removeToCart(group, index): void {
     this.removeItem.emit({ group, index });
-}
+  }
 
-get subTotal(): Observable<number> {
+  get subTotal(): Observable<number> {
     return this.cartService.form.get('items')
-    .valueChanges
-    .pipe(
+      .valueChanges
+      .pipe(
         map((items: Cart[]) => {
-            return items.reduce((acc, curr) => {
-                return acc + curr.price * curr.quantity;
-            }, 0)
+          return items.reduce((acc, curr) => {
+            return acc + curr.price * curr.quantity;
+          }, 0)
         }),
-        tap((total) => this.tax$.next(total* 0.1)),
+        tap(total => this.tax$.next(total* 0.1)),
         startWith(0),
         share()
-    )
-}
+      )
+  }
 
-get total(): Observable<number> {
+  get total(): Observable<number> {
     return combineLatest([this.subTotal, this.tax$])
-    .pipe(
+      .pipe(
         map(([subTotal, tax]) =>  subTotal - tax ),
         startWith(0)
-    )
-}
+      )
+  }
 
-submitForm() {
+  submitForm(): void {
     this.pay.emit([this.displaySubTotal, this.tax$.value, this.displayTotal]);
-}
+  }
 
-}   
+}
