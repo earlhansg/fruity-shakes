f02d60066b6b0adda83bd212ca6367dcac74944e
Earl Hans Genoso <earlhansg@github.com>
2020-09-20 12:19:06 +0800

    #26 Clean code in RestService

--- public/app/src/app/shared/service/rest/rest.service.ts
+++ public/app/src/app/shared/service/rest/rest.service.ts
@@ -1,19 +1,21 @@
 import { HttpClient } from '@angular/common/http';
-// rxjs
+
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-// enum 
+
 import { HttpMethodEnum } from '@app/shared/enums';
 
 
 export abstract class RestService {
 
-  constructor(private http: HttpClient,
-              protected baseUrl: string) {}
+
+  constructor(
+    private http: HttpClient,
+    protected baseUrl: string
+  ) {}
 
   protected request(relativeUrl: string, method: HttpMethodEnum, data?: any): Observable<any> {
-    const url     = this.baseUrl + relativeUrl;
-
+    const url = this.baseUrl + relativeUrl;
     return this.http[method](url, data)
       .pipe(catchError(err => throwError(err)));
   }
