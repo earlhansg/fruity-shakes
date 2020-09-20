7bdb3656d655b1b19f2ab99caef94432be358693
Earl Hans Genoso <earlhansg@github.com>
2020-09-20 12:09:06 +0800

    #26 Clean code in ShakeService

--- public/app/src/app/order/shared/services/shake.service.ts
+++ public/app/src/app/order/shared/services/shake.service.ts
@@ -4,20 +4,27 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { RestService } from '@shared/service';
-// enum
+
 import { HttpMethodEnum } from '@shared/enums';
-// model
+
 import { Order } from '@app/order/shared/models/order.model';
 
-@Injectable({ providedIn: 'root' })
+
+@Injectable({
+  providedIn: 'root'
+})
 export class ShakeService extends RestService {
+
   url = '/shake';
 
-  constructor(http: HttpClient,
-              @Inject('API_URL') protected baseUrl: string) { super(http, baseUrl); }
+
+  constructor(
+    http: HttpClient,
+    @Inject('API_URL') protected baseUrl: string
+  ) { super(http, baseUrl) }
 
   getShakes(): Observable<Order[]> {
-    return this.request(this.url, HttpMethodEnum.GET); 
+    return this.request(this.url, HttpMethodEnum.GET);
   }
 
 }
