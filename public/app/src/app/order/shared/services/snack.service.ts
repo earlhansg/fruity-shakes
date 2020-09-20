a8c407b979692837ad5192da11fdaa22852c4fed
Earl Hans Genoso <earlhansg@github.com>
2020-09-20 12:11:43 +0800

    #26 Clean code in SnackService

--- public/app/src/app/order/shared/services/snack.service.ts
+++ public/app/src/app/order/shared/services/snack.service.ts
@@ -4,20 +4,27 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { RestService } from '@shared/service';
-// enum
+
 import { HttpMethodEnum } from '@shared/enums';
-// model
+
 import { Order } from '@order/shared/models/order.model';
 
-@Injectable({ providedIn: 'root' })
+
+@Injectable({
+  providedIn: 'root'
+})
 export class SnackService extends RestService {
+
   url = '/snack';
 
-  constructor(http: HttpClient,
-              @Inject('API_URL') protected baseUrl: string) { super(http, baseUrl); }
+
+  constructor(
+    http: HttpClient,
+    @Inject('API_URL') protected baseUrl: string
+  ) { super(http, baseUrl) }
 
   getSnacks(): Observable<Order[]> {
-    return this.request(this.url, HttpMethodEnum.GET); 
+    return this.request(this.url, HttpMethodEnum.GET);
   }
 
 }
