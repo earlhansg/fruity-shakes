f8ed16f54e77f51d7711a2f0a775754e8e8fa49c
Earl Hans Genoso <earlhansg@github.com>
2020-09-20 12:01:17 +0800

    #26 Clean code in CartService

--- public/app/src/app/order/shared/services/cart.service.ts
+++ public/app/src/app/order/shared/services/cart.service.ts
@@ -1,59 +1,63 @@
 import { Injectable } from '@angular/core';
-import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms';
-// model
+import { FormGroup, FormControl, FormArray, FormBuilder, AbstractControl } from '@angular/forms';
+
 import { Cart } from '@order/shared/models/cart.model';
 
-@Injectable({ providedIn: 'root' })
+
+@Injectable({
+  providedIn: 'root'
+})
 export class CartService {
 
-form = this.fb.group({
+  form = this.fb.group({
     items: this.fb.array([])
-})
+  })
 
-constructor(private fb: FormBuilder) {}
 
-createItem(item) {
+  constructor(private fb: FormBuilder) {}
+
+  createItem(item: Cart): FormGroup {
     return new FormGroup({
-        _id: new FormControl(item._id),
-        quantity: new FormControl(item.quantity || 1),
-        price: new FormControl(item.price),
-        name: new FormControl(item.name)
+      _id: new FormControl(item._id),
+      quantity: new FormControl(item.quantity || 1),
+      price: new FormControl(item.price),
+      name: new FormControl(item.name)
     });
-}
+  }
 
-addtoCart(item: Cart) {
+  addtoCart(item: Cart): void {
     const control = this.form.get('items') as FormArray;
     control.push(this.createItem(item));
-}
+  }
 
-get items() {
+  get items(): AbstractControl[] {
     return (this.form.get('items') as FormArray).controls;
-}
+  }
 
-existInCart(id: string, details = false) {
+  existInCart(id: string, details = false): any {
     const cartItems = this.form.get('items').value;
     if(!details) {
-        return Boolean(cartItems.find((item: Cart) => item._id === id));
+      return Boolean(cartItems.find((item: Cart) => item._id === id));
     }
     else return cartItems.find((item: Cart) => item._id === id);
-}
+  }
 
-itemIndex(id: string) {
+  itemIndex(id: string): number {
     const cartItems = this.form.get('items').value;
     return cartItems.findIndex((item: Cart) => item._id === id);
-}
+  }
 
-resetCartForm() {
-    const control = <FormArray>this.form.controls['items'];
+  resetCartForm(): void {
+    const control = this.form.controls.items as FormArray;
     for(let i = control.length-1; i >= 0; i--) {
-        control.removeAt(i)
+      control.removeAt(i)
     }
-}
+  }
 
-updateItemQuantity(value: number, index: number) {
+  updateItemQuantity(value: number, index: number): void {
     const quantity = value + 1;
-    const control = (this.form.controls['items'] as FormArray).at(index);
+    const control = (this.form.controls.items as FormArray).at(index);
     control.patchValue({quantity});
-}
+  }
 
 }
