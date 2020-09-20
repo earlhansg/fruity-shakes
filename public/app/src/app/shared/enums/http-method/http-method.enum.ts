c1e5cf1b6d2b09d1acac65093e523e44157dcfb7
Earl Hans Genoso <earlhansg@github.com>
2020-09-20 12:17:14 +0800

    #26 Clean code in HttpMethodEnum

--- public/app/src/app/shared/enums/http-method/http-method.enum.ts
+++ public/app/src/app/shared/enums/http-method/http-method.enum.ts
@@ -1,6 +1,6 @@
 export enum HttpMethodEnum {
-    POST  = 'post',
-    PUT   = 'put',
-    GET   = 'get',
-    DEL   = 'delete'
+  POST  = 'post',
+  PUT   = 'put',
+  GET   = 'get',
+  DEL   = 'delete'
 }
